5e1aee47c8a07e8201537f1f95573043f025e3dd
Alex1445085 <n1445085@yandex.ru>
2024-10-01 15:52:17 +0300

    initial commit

---
+++ src/main/java/pro/sky/hw211market/Hw211MarketApplication.java
@@ -0,0 +1,11 @@
+package pro.sky.hw211market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw211MarketApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Hw211MarketApplication.class, args);
+	}
+}
